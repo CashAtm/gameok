4e34d4fc7137630840de16ae74312be847d129ad
av900137 <av900137@aldine-isd.org>
2023-10-19 10:52:38 -0500

    scriptable objects

---
+++ New Unity Project/Assets/Scripts/Stats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Enemy")]
+public class Stats : ScriptableObject
+{
+   public string name;
+   public int health;
+   public int attack;
+   public int speed;
+}
