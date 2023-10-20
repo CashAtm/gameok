24341d16f630b852126fa2478b2665a1bc1b804a
av900137 <av900137@aldine-isd.org>
2023-10-20 10:54:54 -0500

    ok

---
+++ New Unity Project/Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+    public Stats test;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = (Stats)ScriptableObject.CreateInstance(typeof(Stats));
+    }
+
+    void update {
+        // if hit by attack, check damage stat
+        // if 0 then die
+        // otherwise subtract damage from health
+    }
+}
--- New Unity Project/Assets/Scripts/Stats.cs
+++ New Unity Project/Assets/Scripts/Stats.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Character", menuName = "Enemy")]
 public class Stats : ScriptableObject
 {
-   public string name;
    public int health;
    public int attack;
    public int speed;
